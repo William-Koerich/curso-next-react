ecd994831e4a9adf3912ba0683b557a64fba8e7a
William Koerich <williamkoerich17@gmail.com>
2021-10-23 11:45:11 -0400

    feat: learning props of component

---
+++ exercicios/components/Titulo.jsx
@@ -0,0 +1,8 @@
+export default function Titulo(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </>
+  )
+}
---
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+export default function usandoTitulo() {
+  return (
+    <Titulo
+      title={'Página de cadastro'}
+      subtitle={'Incluir, alterar e excluir coisas!'}
+    />
+  )
+}
