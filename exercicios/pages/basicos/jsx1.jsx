b8e7f4f800535165d78801f1ab5ab846be355ff3
William Koerich <williamkoerich17@gmail.com>
2021-10-23 10:38:17 -0400

    feat: learning how jsx works

---
+++ exercicios/pages/basicos/jsx1.jsx
@@ -0,0 +1,10 @@
+/* JSX */
+export default function jsx1() {
+   return (
+      <div>
+         <h1>
+            JSX #01
+         </h1>
+      </div>
+   )
+}
---
+++ exercicios/pages/basicos/jsx3.jsx
@@ -0,0 +1,9 @@
+export default function jsx3() {
+  return (
+    <div>
+      <span className="teste">
+        <h1 className="opa">JSX #03</h1>
+      </span>
+    </div>
+  )
+}
